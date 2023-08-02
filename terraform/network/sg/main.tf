f24be7ceac0212df2c6365985e7cf08698b16eff
yellowpenguincookie <yurijjjung@gmail.com>
2023-08-02 14:12:16 +0900

    20230802

--- terraform/network/sg/main.tf
+++ terraform/network/sg/main.tf
@@ -1,13 +1,13 @@
 
 #SSH Security group
 resource "aws_security_group" "project04_ssh_sg" {
-    name   = "project04_ssh_sg"
+    name   = "Project04 SSH Accept"
     description = "security group for ssh server"
-    vpc_id = project04_vpc
+    vpc_id = data.terraform_remote_state.project04_vpc.outputs.vpc_id
 
 
     ingress {
-        description = "For SSH port"
+        description = "For SSH accept"
         protocol    = "tcp"
         from_port   = 22
         to_port     = 22
@@ -22,24 +22,24 @@ resource "aws_security_group" "project04_ssh_sg" {
     }
 
     tags = {
-        Name = "project04_ssh_sg"
-        Description = "SSH security group"
+        Name = "Project04 SSH Accept"
+        Description = "for SSH accept"
     }
 }
 
 
-#WEB Security group
-resource "aws_security_group" "project04_web_sg" {
-    name        = "project04_web_sg"
-    description = "security group for WEB server"
-    vpc_id      = project04_vpc
+#WEB Security group - HTTP 8080
+resource "aws_security_group" "project04_web_sg_http" {
+    name        = "Project04 WEB Accept-HTTP"
+    description = "sg for HTTP accept"
+    vpc_id      = data.terraform_remote_state.project04_vpc.outputs.vpc_id
 
 
     ingress {
         description = "For WEB port"
         protocol    = "tcp"
-        from_port   = 80
-        to_port     = 80
+        from_port   = 8080
+        to_port     = 8080
         cidr_blocks = ["0.0.0.0/0"]
     }
 
@@ -51,10 +51,46 @@ resource "aws_security_group" "project04_web_sg" {
     }
 
     tags = {
-        Name = "project04_web_sg"
-        Description = "WEB security group"
+        Name = "Project04 WEB Accept-HTTP"
+        Description = "sg for HTTP accept"
     }
 }
 
+
+#WEB Security group - HTTP 80, 443
+resource "aws_security_group" "project04_web_sg_https" {
+    name        = "Project04 WEB Accept-HTTPS"
+    description = "sg for HTTP accept"
+    vpc_id      = data.terraform_remote_state.project04_vpc.outputs.vpc_id
+
+
+    ingress {
+        description = "For WEB port"
+        protocol    = "tcp"
+        from_port   = 80
+        to_port     = 80
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "For WEB port"
+        protocol    = "tcp"
+        from_port   = 443
+        to_port     = 443
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        protocol    = "-1"
+        from_port   = 0
+        to_port     = 0
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "Project04 WEB Accept-HTTPS"
+        Description = "sg for HTTP accept"
+    }
+}
 
 
--- terraform/network/vpc/main.tf
+++ terraform/network/vpc/main.tf
@@ -92,24 +92,25 @@ resource "aws_nat_gateway" "project04_nat" {
 }
 
 
-
 #-------------Public Route Table----------------
 # AWS 에서 VPC 를 생성하면 자동으로 Route table 이 생긴다.
 # aws_default_route_table 은 route table 을 만들지 않고 VPC 가 만든
 # 기본 route table 을 가져와서 terraform 이 관리할 수 있게 함. 
 # local 및 IGW (인터넷게이트웨이) 와 연결됨 
 
+
 resource "aws_default_route_table" "project04_public_rt" {
 	default_route_table_id = aws_vpc.project04_vpc.default_route_table_id
 	
-		route {
-			cidr_block = "0.0.0.0/0"
-			gateway_id = aws_internet_gateway.project04_igw.id
+	route {
+		cidr_block = "0.0.0.0/0"
+		gateway_id = aws_internet_gateway.project04_igw.id
 		}
 	tags = {
 		Name = "project04_public_route_table"
 	}
 }
+
 
 #디폴트 라우터를 퍼블릭 서브넷에 연결 
 resource "aws_route_table_association" "project04_public_rta_2a" {
@@ -123,32 +124,44 @@ resource "aws_route_table_association" "project04_public_rta_2c" {
 }
 
 
+
 #-------------Private Route Table----------------
 # 프라이빗 라우트 생성 
-resource "aws_route_table" "project04_private_rt" {
+resource "aws_route_table" "project04_private_rt1" {
 	vpc_id = aws_vpc.project04_vpc.id
 	tags = {
-		Name = "project04_private_route_table"
+		Name = "project04_private_route_table01"
+	}
+}
+
+resource "aws_route_table" "project04_private_rt2" {
+	vpc_id = aws_vpc.project04_vpc.id
+	tags = {
+		Name = "project04_private_route_table02"
 	}
 }
 
 # 프라이빗 라우터를 프라이빗 서브넷에 연결 
 resource "aws_route_table_association" "project04_private_rta_2a" {
 	subnet_id = aws_subnet.project04_private_subnet2a.id
-	route_table_id = aws_route_table.project04_private_rt.id
+	route_table_id = aws_route_table.project04_private_rt1.id
 }
 
 resource "aws_route_table_association" "project04_private_rta_2c" {
 	subnet_id = aws_subnet.project04_private_subnet2c.id
-	route_table_id = aws_route_table.project04_private_rt.id
+	route_table_id = aws_route_table.project04_private_rt2.id
 }
 
 #퍼블릭으로 나갈 수 있게 NAT gateway 를 붙여줘야 함. 
-resource "aws_route" "project04_private_rt_table" {
-	route_table_id = aws_route_table.project04_private_rt.id
+resource "aws_route" "project04_private_rt_table1" {
+	route_table_id = aws_route_table.project04_private_rt1.id
 	destination_cidr_block = "0.0.0.0/0"
 	nat_gateway_id = aws_nat_gateway.project04_nat.id 
 }
 
-
+resource "aws_route" "project04_private_rt_table2" {
+	route_table_id = aws_route_table.project04_private_rt2.id
+	destination_cidr_block = "0.0.0.0/0"
+	nat_gateway_id = aws_nat_gateway.project04_nat.id 
+}
 
