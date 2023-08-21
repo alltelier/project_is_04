1ff899c61220f1d17055181c6f7dae5b19c2ee74
yellowpenguincookie <yurijjjung@gmail.com>
2023-08-21 16:05:46 +0900

    20230821

--- terraform/ec2/alb/main.tf
+++ terraform/ec2/alb/main.tf
@@ -2,18 +2,15 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-
 #로드밸런서 생성 
 resource "aws_lb" "project04-lb" {
 	name			= "project04-lb"
 	load_balancer_type	= "application"
 #로드밸런스 생성되는 vpc의 서브넷 
-	subnets			= ["subnet-07520d03716ae0e01","subnet-0b6b4369668ab5265"]
+	subnets			= [for subnet in aws_subnet.public : subnet.id]
 #로드밸런스에 사용할 보안 그룹들 
-	security_groups		= [data.aws_security_group.project04_web_sg.id,data.aws_security_group.project04_alb_sg.id]
+	security_groups		= [data.terraform_remote_state.project04_sg.outputs.project04_alb]
 } 
-
-
 
 #ALB 리스너 - HTTP:80
 resource "aws_lb_listener" "http" {
@@ -28,7 +25,7 @@ resource "aws_lb_listener" "http" {
 
 }
 #로드밸런서 리스너 룰 구성
-#resource "aws_lb_listener_rule" "HTTP80" {
+#resource "aws_lb_listener_rule" "http" {
 #	listener_arn	= aws_lb_listener.http.arn
 #	priority		= 100
 #
@@ -46,7 +43,7 @@ resource "aws_lb_listener" "http" {
 
 
 #ALB 리스너 HTTP:8080
-resource "aws_lb_listener" "asg-jenkins" {
+resource "aws_lb_listener" "HTTP" {
   load_balancer_arn = aws_lb.project04-lb.arn
   port				= 8080
   protocol			= "HTTP"
@@ -58,7 +55,7 @@ resource "aws_lb_listener" "asg-jenkins" {
 
 }
 #로드밸런서 리스너 룰 구성
-#resource "aws_lb_listener_rule" "HTTP8080" {
+#resource "aws_lb_listener_rule" "HTTP" {
 #	listener_arn	= aws_lb_listener.http.arn
 #	priority		= 100
 #
@@ -114,24 +111,6 @@ resource "aws_lb_target_group" "asg-jenkins" {
 
 
 #정보 가져 오기
-data "aws_vpc" "project04-vpc"  {
-	id = "vpc-0ef5fd852b154d136"
-}
-data "aws_subnets" "project04-vpc" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.project04-vpc.id]
-  }
-}
-
-data "aws_security_group" "project04_alb_sg" {
-    id = "sg-0151c526c8195891f"
-}
-
-data "aws_security_group" "project04_web_sg" {
-    id = "sg-04a9d5c5086565b93"
-}
-
 data "template_file" "web_output" {
 	template = file("${path.module}/web.sh")
 	vars = {
